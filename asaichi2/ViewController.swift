890b1306eedfc15b414ff11d5afe5735baaa4877
ShomaYamaguchi <b1019125@fun.ac.jp>
2021-10-08 16:06:35 +0900

    Initial Commit

---
+++ asaichi2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  asaichi2
+//
+//  Created by AICT201305 FUN on 2021/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
